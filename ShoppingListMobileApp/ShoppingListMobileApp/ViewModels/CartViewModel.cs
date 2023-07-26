22a4dd8cf52708426738652698450c7b7e188a94
Başar Çelebi <75533431+celebibasar@users.noreply.github.com>
2023-07-26 03:36:21 +0300

    Cart Page Updated

---
+++ ShoppingListMobileApp/ShoppingListMobileApp/ViewModels/CartViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.Maui.Controls;
+
+namespace ShoppingListMobileApp.ViewModels
+{
+    public class CartViewModel : BindableObject
+    {
+        private ObservableCollection<CartItem> _cartItems;
+        public ObservableCollection<CartItem> CartItems
+        {
+            get { return _cartItems; }
+            set
+            {
+                _cartItems = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public CartViewModel()
+        {
+            // Örnek ürünleri ekleyelim.
+            CartItems = new ObservableCollection<CartItem>
+            {
+                new CartItem { ProductName = "Ürün 1", Quantity = 2, ProductImage = "product1.png" },
+                new CartItem { ProductName = "Ürün 2", Quantity = 1, ProductImage = "product2.png" },
+                new CartItem { ProductName = "Ürün 3", Quantity = 3 , ProductImage = "product3.png"}
+            };
+        }
+    }
+
+    public class CartItem
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public string ProductImage { get; set; }
+    }
+}
--- ShoppingListMobileApp/ShoppingListMobileApp/Views/CartPageView.xaml.cs
+++ ShoppingListMobileApp/ShoppingListMobileApp/Views/CartPageView.xaml.cs
@@ -1,9 +1,39 @@
-﻿namespace ShoppingListMobileApp;
+﻿using Microsoft.Maui.Controls;
+using ShoppingListMobileApp.ViewModels;
 
-public partial class CartPageView : ContentPage
+namespace ShoppingListMobileApp
 {
-	public CartPageView()
-	{
-		InitializeComponent();
-	}
+    public partial class CartPageView : ContentPage
+    {
+        public CartPageView()
+        {
+            InitializeComponent();
+        }
+
+        private CartViewModel ViewModel => (CartViewModel)BindingContext;
+
+        private void OnDecreaseClicked(object sender, EventArgs e)
+        {
+            var cartItem = (CartItem)((Button)sender).CommandParameter;
+            if (cartItem.Quantity > 1)
+                cartItem.Quantity--;
+        }
+
+        private void OnIncreaseClicked(object sender, EventArgs e)
+        {
+            var cartItem = (CartItem)((Button)sender).CommandParameter;
+            cartItem.Quantity++;
+        }
+
+        private void OnRemoveClicked(object sender, EventArgs e)
+        {
+            var cartItem = (CartItem)((Button)sender).CommandParameter;
+            ViewModel.CartItems.Remove(cartItem);
+        }
+
+        private void OnConfirmClicked(object sender, EventArgs e)
+        {
+            // Sepeti onaylama işlemlerini burada yapabilirsiniz.
+        }
+    }
 }
