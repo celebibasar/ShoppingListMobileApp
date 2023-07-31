cf898ea2b2d53cd64947bf133f6f5c59af158842
Başar Çelebi <75533431+celebibasar@users.noreply.github.com>
2023-07-31 13:45:35 +0300

    Notifications Page Updated

---
+++ ShoppingListMobileApp/ShoppingListMobileApp/Views/NotificationsPageView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Maui.Controls;
+using ShoppingListMobileApp.Models;
+using ShoppingListMobileApp.ViewModels;
+using System;
+using Notification = ShoppingListMobileApp.Models.Notification;
+
+
+namespace ShoppingListMobileApp
+{
+    public partial class NotificationsPageView : ContentPage
+    {
+        public NotificationsPageView()
+        {
+            InitializeComponent();
+            
+        }
+        private void ShowButton_Clicked(object sender, EventArgs e)
+        {
+            if (sender is Button button && button.BindingContext is Notification notification)
+            {
+                DisplayAlert(notification.Title, notification.Message, "OK");
+            }
+        }
+
+    }
+}
